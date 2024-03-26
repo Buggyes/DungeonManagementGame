e79115b0ba012dd120eb5046e9bf61bc8500a8b5
Matheus Eduardo dos Santos <matheusthja1@gmail.com>
2024-03-26 06:50:39 -0300

    Created the class for the minions

---
+++ Minion.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dungeon_Management_Game
+{
+    public enum MinionType
+    {
+        skeleton, imp, golem, gargoyle, wraith, fishman
+    }
+    public class Minion
+	{
+        public float maximumHealth { get; private set; }
+        public float currentHealth { get; private set; }
+        public float damage { get; private set; }
+        public double salary { get; set; }
+        public MinionType type { get; private set; }
+        public Minion(float maximumHealth, float damage, MinionType type, double salary)
+        {
+            this.maximumHealth = maximumHealth;
+            this.damage = damage;
+            this.type = type;
+            this.salary = salary;
+        }
+	}
+}
